ff6cbbcd598115cedddf5c616baecadecff3f616
Sydney Micklas <31852294+smicklas@users.noreply.github.com>
2021-04-18 20:23:26 -0400

    Added buttons for selecting difficulty, styling

--- league-quiz/src/components/AppContainer.js
+++ league-quiz/src/components/AppContainer.js
@@ -1,130 +1,114 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Image from './Image';
-import QuizContent from './QuizContent';
-import '../stylesheets/styles.css'
-  class AppContainer extends React.Component {
-    //TODO - add loader after kicking off the data parsing/retrieving 
+import React from "react";
+import ReactDOM from "react-dom";
+import Image from "./Image";
+import QuizContent from "./QuizContent";
+import "../stylesheets/styles.css";
+class AppContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sourceData: [],
+      questionData: null,
+      showIntro: true,
+      dataLoaded: false,
+      numberOfQuestions: null,
+      feedBackMessage: null,
+    };
+  }
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        sourceData: [],
-        questionData: null,
-        showIntro: true,
-        dataLoaded: false
-      };
-    }
-
-    startGame = () => {
+  startGame = () => {
+    if (this.state.numberOfQuestions == null) {
+      this.setState({feedbackMessage: "Please select a difficulty (number of questions)."})
+      console.log("select a question number first");
+    } else {
       const scope = this;
-      fetch('mobafire/Abilities.html')
-      .then(function(response){
-        return response.text();
-      }
-        ).then(function (data) {
-          scope.createAbilitiesJSON(data);
+      const getAbilityInfoUrl = "https://j094n3z36d.execute-api.us-east-2.amazonaws.com/prod/abilities/" + this.state.numberOfQuestions;
+      //TODO -add error handling
+      this.setState({ feedbackMessage: "Getting data... hang tight."})
+      fetch(getAbilityInfoUrl)
+        .then((response) => response.json())
+        .then(function (data) {
+          scope.setState({ sourceData: data.abilities });
+          scope.setState({ showIntro: false });
         });
-    }
-
-    createAbilitiesJSON(source) {
-      var temp = [];
-      var count = (content) => {
-        //Regex for getting ability name
-        //const re = /(?<="ability-list__item__name">).*?(?=\n*<span)/g
-  
-        //Regex for getting entire a block 
-        const re = /(?<=<a href="https:\/\/www.mobafire.com\/league-of-legends\/ability\/).*?(?=<\/a>)/gs
-        return ((content || '').match(re) || [])
-      }
-      var raw = count(source);
-      raw.forEach(element => {
-          //split up further 
-          temp.push(this.getAbilityInfo(element));
+      if(this.state.numberOfQuestions == "all"){  
+        const getAbilityCountUrl = "https://j094n3z36d.execute-api.us-east-2.amazonaws.com/prod/abilitiesCount"
+        fetch( getAbilityCountUrl)
+          .then((response) => response.json())
+          .then(function (data) {
+            scope.setState({ numberOfQuestions: data.count});
       });
-      this.setState({showIntro : false});
-      if(temp != null){
-        this.setState({sourceData: temp});
-      }else{
-        console.log("Something went wrong trying to get the data."); 
-      }
-    }
-
-    //TODO - rename data entry from temp. need to make sure the datap parsing is done before we move on
-    //TODO - backend for the file parsing 
-    getAbilityInfo(content){
-      var temp = {"ability": null, "champion": null, "control": null, "image": null, "description": null, "isPNG": false };
-
-      //Check for passives
-      const re_passive = /(passive-symbol)/g
-      var passive_check = (content).match(re_passive);
-      var isPassive = false;
-      if(passive_check){
-          isPassive = true; 
-      }
-      //Regex for getting ability name
-      const re_ability = /(?<="ability-list__item__name">).*?(?=\n* *<span)/gs
-      temp.ability = (content).match(re_ability)[0];
-  
-      //Regex for champion name 
-      const re_champion = /(?<=champ=").*?(?=\n* *")/g
-      temp.champion = (content).match(re_champion)[0];
-
-      //Regex for default key of ability
-      if(isPassive){
-          temp.control = "Passive"
-      }else{
-          const re_control = /(?<="ability-list__item__keybind">).*?(?=\ *<\/div>)/g
-          temp.control = (content).match(re_control)[0];
-      }
-  
-      if(isPassive){
-          const re_image =  /(?<=passive-symbol\.webp".*?)(?<=<img src="\.\/Abilities __ LoL Champion Abilities_files\/).*?(?=")/gs;
-          temp.image = (content).match(re_image)[0];
-       }else{
-          const re_image = /(?<=(<div class="ability-list__item__keybind">.<\/div>).*?)(?<=<img src=".\/Abilities __ LoL Champion Abilities_files\/).*?(?=" alt)/gs
-          temp.image = (content).match(re_image)[0];
-      }
-
-      //Check if it's a png 
-      //TODO - sometimes the ability is a webp but the champ img is a png 
-      const re_png = /(.png)/g
-      var png_check = (temp.image).match(re_png);
-      if(png_check != null){
-        temp.isPNG = true;
-      }else{
-        temp.isPNG = false;
-      }
-  
-      const re_description = /(?<=<span class="desc">)(.*?)(?=<div)/gs
-      temp.description = (content).match(re_description)[0];
-
-      //TODO - some are .pngs.... 
-
-      const apostrophe_code = "&#39;";
-      temp.champion = temp.champion.replace(apostrophe_code, "'");
-
-      var temp_sourceData = this.state.sourceData;
-      temp_sourceData.push(temp);
-      this.setState({sourceData : temp_sourceData});
-      return temp;
-    }
-
-    render() {
-      return (
-        <div className="app-container"> 
-          {this.state.showIntro ?
-            <div className="">
-            <button onClick={this.startGame}> Start it up </button>
-            <p> hold on a sec.... loading the goods</p>
-            </div>
-          : 
-            <QuizContent data={this.state} />  
-          }
-        </div>
-      );
     }
   }
-  
-  
-  export default AppContainer;
+  };
+
+  setDifficulty = (numQuestions) => {
+    this.setState({feedbackMessage: null})
+    const scope = this;
+    //Find out what the current total number of abilities is 
+    this.setState({ numberOfQuestions: numQuestions });
+  };
+
+  render() {
+    return (
+      <div className="app-container">
+        {this.state.showIntro ? (
+          <div>
+            <img
+              className="quiz-logo"
+              alt="League of Legends Ability Quiz"
+              src="../Logo.png"
+            />
+            {this.state.feedbackMessage ? <p className="feedback">{this.state.feedbackMessage}</p> : null}
+            <div className="difficulty-buttons-container">
+              <div className={"difficulty-button-wrapper" + (this.state.numberOfQuestions == 10 ? " selected" : "")}  onClick={() => {this.setDifficulty(10);}}>
+                <img className="difficulty-button" role="button" src="../Iron.png"/>
+                <p>10 Questions</p>
+              </div>
+              <div className={"difficulty-button-wrapper" + (this.state.numberOfQuestions == 20 ? " selected" : "")} onClick={() => {this.setDifficulty(20);}}>
+                <img className="difficulty-button" role="button" src="../Bronze.png"/>
+                <p>20 Questions</p>
+              </div>
+              <div className={"difficulty-button-wrapper" + (this.state.numberOfQuestions == 30 ? " selected" : "")} onClick={() => { this.setDifficulty(30);}}>
+                <img className="difficulty-button" role="button" src="../Silver.png"/>
+                <p>30 Questions</p>
+              </div>
+              <div className={"difficulty-button-wrapper" + (this.state.numberOfQuestions == 50 ? " selected" : "")} onClick={() => {this.setDifficulty(50);}}>
+                <img className="difficulty-button" role="button"src="../Gold.png"/>
+                <p>50 Questions</p>
+              </div>
+              <div className={"difficulty-button-wrapper" + (this.state.numberOfQuestions == 100 ? " selected" : "")} onClick={() => { this.setDifficulty(100);}}>
+                <img className="difficulty-button" role="button" src="../Platinum.png"/>
+                <p>100 Questions</p>
+              </div>
+              <div className={"difficulty-button-wrapper" + (this.state.numberOfQuestions == 250 ? " selected" : "")} onClick={() => { this.setDifficulty(250);}}>
+                <img className="difficulty-button" role="button" src="../Diamond.png"/>
+                <p>250 Questions</p>
+              </div>
+              <div className={"difficulty-button-wrapper" + (this.state.numberOfQuestions == 500 ? " selected" : "")} onClick={() => { this.setDifficulty(500); }}>
+                <img className="difficulty-button" role="button" src="../Master.png"/>
+                <p>500 Questions</p>
+              </div>
+              <div className={"difficulty-button-wrapper" + (this.state.numberOfQuestions == 700 ? " selected" : "")} onClick={() => { this.setDifficulty(700);}}>
+                <img className="difficulty-button" role="button" src="../Grandmaster.png" />
+                <p>700 Questions</p>
+              </div>
+              <div className={"difficulty-button-wrapper" + (this.state.numberOfQuestions == "all" ? " selected" : "")} onClick={() => { this.setDifficulty("all");}}>
+                <img className="difficulty-button" role="button" src="../Challenger.png" />
+                <p>ALL</p>
+              </div>
+            </div>
+            <button className="primary-button large" onClick={this.startGame}>
+              {" "}
+              START{" "}
+            </button>
+          </div>
+        ) : (
+          <QuizContent data={this.state} />
+        )}
+      </div>
+    );
+  }
+}
+
+export default AppContainer;
--- league-quiz/src/components/QuizContent.js
+++ league-quiz/src/components/QuizContent.js
@@ -7,35 +7,74 @@ class QuizContent extends React.Component {
     this.state = {
       sourceData : this.props.data.sourceData, 
       questionData: [],
-      correctAnswers: [],
-      currentQuestion: 1,
+      correctAnswers: 0,
+      currentQuestion: 0,
       attempts: 0,
       showAnswer: false,
-      randomNum: 0,
-      isCorrect : null
+      isCorrect : null,
+      totalQuestions : this.props.data.numberOfQuestions,
+      userAnswer: ""
     }
   }
 
+  //TODO - more descriptive variables! 
+
   componentDidMount(){
-    //Select first champ 
-    //TODO - give more descriptive name
-    let random_num = Math.ceil(Math.random() * this.state.sourceData.length);
-    this.setState({randomNum : 108});
+
   }
 
+  //https://redstapler.co/find-diff-between-2-strings-with-javascript/
+  checkDifference = (str1, str2) => { 
+    var dist = 0;
+    for (var i = 0, j = Math.max(str1.length, str2.length); i < j; i++) {
+      if (!str1[i] || !str2[i] || str1[i] !== str2[i]) {
+        dist++;
+      }
+    }
+    return dist;
+  }
   checkAnswer = () => {
-    //for now it's just correct, not gonna do any checkin yet
-    this.setState({isCorrect: true});
+    this.setState({attempts : this.state.attempts + 1})
+    //convert to lower cases 
+    let cleansedAbilityName = this.state.sourceData[this.state.currentQuestion].name.toLowerCase();
+    let cleansedUserInput = this.state.userAnswer.toLowerCase();
+
+    //remove spaces
+    cleansedUserInput = cleansedUserInput.replace(/\s/g, '');
+    cleansedAbilityName = cleansedAbilityName.replace(/\s/g, '');
+
+    let differenceCheck = "";
+    differenceCheck = this.checkDifference(cleansedAbilityName, cleansedUserInput)
+
+    if(cleansedUserInput == cleansedAbilityName || differenceCheck < 3){
+      this.setState({correctAnswers: this.state.correctAnswers + 1});
+      this.setState({isCorrect: true});
+      this.setState({showAnswer : true});
+    }else{
+      this.setState({isCorrect: false});
+      this.setState({showAnswer : false});
+    }
+  }
+
+  showAnswer = () => { 
+    this.setState({isCorrect: null});
     this.setState({showAnswer : true});
   }
 
   nextPage = () => {
-    this.setState({isCorrect: null});
-    this.setState({showAnswer: false});
-    this.setState({currentQuestion : this.state.currentQuestion+1});
-    //remove the previous entry from the array
-    let random_num = Math.ceil(Math.random() * this.state.sourceData.length);
-    this.setState({randomNum : random_num});
+    if(this.state.currentQuestion + 1  == this.state.totalQuestions){
+      console.log("all done!")
+    }else{
+      this.setState({isCorrect: null});
+      this.setState({showAnswer: false});
+      this.setState({userAnswer: ""});
+      this.setState({attempts: 0});
+      this.setState({currentQuestion : this.state.currentQuestion+1});
+    }
+  }
+
+  handleChange = (e) =>{
+    this.setState({userAnswer: e.target.value});
   }
 
   render() {
@@ -43,40 +82,35 @@ class QuizContent extends React.Component {
     return (
         <div className="quiz-content">
           {this.state.isCorrect ?
-            <p className="feedback-text">Correct</p>
+            <p className="feedback-text correct">Correct</p>
           :
-            this.state.attempts > 0 ?
-              <p className="feedback-text">Incorrect</p>
+            this.state.attempts > 0 && !this.state.showAnswer ?
+              <p className="feedback-text incorrect">Incorrect</p>
             :
               <p className="feedback-text"></p>
           }
           
           {this.state.showAnswer ? 
             <div className="">
-              <img className="ability-image small" src={`/mobafire/Abilities__LoLChampionAbilities_Files/${this.state.sourceData[this.state.randomNum].image}`}/>
-              <h2><span>{this.state.currentQuestion}/{this.state.sourceData.length}</span></h2>
-              <p className="ability-title">{this.state.sourceData[this.state.randomNum].ability} - {this.state.sourceData[this.state.randomNum].control}</p>
+              <img className="ability-image small" src={`${this.state.sourceData[this.state.currentQuestion].image}`}/>
+              <h1>{this.state.currentQuestion + 1}/{this.state.totalQuestions}</h1>
+              <p className="ability-title">{this.state.sourceData[this.state.currentQuestion].name} - {this.state.sourceData[this.state.currentQuestion].control}</p>
               <hr className="gradient-decor"/>
               <div className="image-container">
               <div className="image-border"></div>
-              {this.state.sourceData[this.state.randomNum].isPNG ?
-                <div className="champion-img" style={{backgroundImage:`url(/mobafire/Abilities__LoLChampionAbilities_Files/${this.state.sourceData[this.state.randomNum].champion.replace("'", "")}.png)`, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}/>
-              : 
-              <div className="champion-img" style={{backgroundImage:`url(/mobafire/Abilities__LoLChampionAbilities_Files/${this.state.sourceData[this.state.randomNum].champion.replace("'", "")}.webp)`, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}/>
-            }
+                <div className="champion-img" style={{backgroundImage:`url(${this.state.sourceData[this.state.currentQuestion].championImage})`, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}/>
               </div>
-              /mobafire/Abilities__LoLChampionAbilities_Files/{this.state.sourceData[this.state.randomNum].champion.replace("'", "")}.webp
-              <div className="ability-champion">{this.state.sourceData[this.state.randomNum].champion}</div>
-              <p className="ability-description" dangerouslySetInnerHTML={{__html: this.state.sourceData[this.state.randomNum].description}}></p>
+              <div className="ability-champion">{this.state.sourceData[this.state.currentQuestion].champion}</div>
+              <p className="ability-description" dangerouslySetInnerHTML={{__html: this.state.sourceData[this.state.currentQuestion].description}}></p>
               <button className="primary-button" onClick={this.nextPage}>NEXT</button>
             </div>
           :
             <div className="">
-              <img className="ability-image" src={`/mobafire/Abilities__LoLChampionAbilities_Files/${this.state.sourceData[this.state.randomNum].image}`}/>
-              <h2><span>{this.state.currentQuestion}/{this.state.sourceData.length}</span></h2>
-              <input type="text"/>
+              <img className="ability-image" src={`${this.state.sourceData[this.state.currentQuestion].image}`}/>
+              <h1>{this.state.currentQuestion + 1}/{this.state.totalQuestions}</h1>
+              <input type="text" onChange={this.handleChange}/>
               <button className="primary-button" onClick={this.checkAnswer}>SUBMIT</button>
-              <button className="secondary-button">I GIVE UP!</button>
+              <button className="secondary-button" onClick={this.showAnswer}>I GIVE UP!</button>
             </div>
           }
         </div>
